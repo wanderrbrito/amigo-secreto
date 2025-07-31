4af015f59407e7e0080455a25c719be98bd1b3ff
wanderrbrito <wanderrbrito@gmail.com>
2025-07-31 20:58:19 -0300

    Alteracao no HTML e criacao das functions

--- app.js
+++ app.js
@@ -6,7 +6,30 @@ function adicionarAmigo() {
     let adcionar = document.querySelector('input').value;
     if (adcionar == '') {
         alert('Por favor, insira um nome');
-    } else {
-        amigos.push(adcionar);
+    } if (adcionar == 'X' || adcionar == 'x') {
+        limparCampo();
+        listaAmigos();
     }
+        amigos.push(adcionar);
+        limparCampo();
+        console.log(amigos);
+} 
+
+function listaAmigos() {  
+    let i = 0;
+    while (i < amigos.length) {
+        let lista = document.querySelector('ul');
+        lista.innerHTML = amigos[i];
+        i++
+    }
+    
+}
+
+function sortearAmigo() {
+    // criar codigo
+}
+
+function limparCampo() { 
+    adcionar = document.querySelector('input');
+    adcionar.value = '';
 }
